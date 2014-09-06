a1c9621ce2bcf3f9883035956dcc9772f9e008cb
Fernando Mendes <devfrmendes@gmail.com>
2014-09-07 00:45:02 +0100

    Finishes navbar and renames java banner

--- js/nav.js
+++ js/nav.js
@@ -3,15 +3,30 @@ $(function() {
     var bodyContainer = $('.container');
     var initialMargin = parseInt( bodyContainer.css('margin-top') );
     var marginFix = initialMargin + nav.height();
+    var navHeight = nav.height() + nav.outerHeight();
 
     $(window).scroll(function() {
-      var navHeight = $( window ).height() - nav.offset().top + nav.height() + nav.outerHeight();
-      if ($(window).scrollTop() > navHeight) {
+      var navPosition = $( window ).height() - nav.offset().top + navHeight;
+      if ($(window).scrollTop() > navPosition) {
         nav.addClass('fixed');
         bodyContainer.css('margin-top', marginFix + 'px');
       } else {
         nav.removeClass('fixed');
         bodyContainer.css('margin-top', initialMargin + 'px');
+        $('.active-link').removeClass('active-link');
       }
+
+    });
+
+
+    nav.find('a').on('click', function() {
+      var top = $('html, body');
+      var self = $(this);
+      top.animate({
+        'scrollTop': $('#' + self.data('target')).offset().top - navHeight
+      }, 1000).promise().done(function() {
+        $('.active-link').removeClass('active-link');
+        self.addClass('active-link');
+      });
     });
 });
