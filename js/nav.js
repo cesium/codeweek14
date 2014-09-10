afbe6fcc359aa0daf0bf28c57fd860ecb9c933e3
Fernando Mendes <devfrmendes@gmail.com>
2014-09-10 23:12:30 +0100

    Removes jQuery calculations when on mobile devices

--- js/nav.js
+++ js/nav.js
@@ -1,32 +1,36 @@
+var activateNav = function() {
+  var nav = $('nav');
+  var bodyContainer = $('.container');
+  var initialMargin = parseInt( bodyContainer.css('margin-top') );
+  var marginFix = initialMargin + nav.height();
+  var navHeight = nav.height() + nav.outerHeight();
+
+  $(window).scroll(function() {
+    var navPosition = $( window ).height() - nav.offset().top + navHeight;
+
+    if ($(window).scrollTop() > navPosition) {
+      nav.addClass('fixed');
+      bodyContainer.css('margin-top', marginFix + 'px');
+    } else {
+      nav.removeClass('fixed');
+      bodyContainer.css('margin-top', initialMargin + 'px');
+      $('.active-link').removeClass('active-link');
+    }
+  });
+
+  nav.find('a').on('click', function() {
+    var top = $('html, body');
+    var self = $(this);
+    top.animate({
+      'scrollTop': $('#' + self.data('target')).offset().top - navHeight
+    }, 1000).promise().done(function() {
+      $('.active-link').removeClass('active-link');
+      self.addClass('active-link');
+    });
+  });
+};
+
 $(function() {
-    var nav = $('nav');
-    var bodyContainer = $('.container');
-    var initialMargin = parseInt( bodyContainer.css('margin-top') );
-    var marginFix = initialMargin + nav.height();
-    var navHeight = nav.height() + nav.outerHeight();
-
-    $(window).scroll(function() {
-      var navPosition = $( window ).height() - nav.offset().top + navHeight;
-      if ($(window).scrollTop() > navPosition) {
-        nav.addClass('fixed');
-        bodyContainer.css('margin-top', marginFix + 'px');
-      } else {
-        nav.removeClass('fixed');
-        bodyContainer.css('margin-top', initialMargin + 'px');
-        $('.active-link').removeClass('active-link');
-      }
-
-    });
-
-
-    nav.find('a').on('click', function() {
-      var top = $('html, body');
-      var self = $(this);
-      top.animate({
-        'scrollTop': $('#' + self.data('target')).offset().top - navHeight
-      }, 1000).promise().done(function() {
-        $('.active-link').removeClass('active-link');
-        self.addClass('active-link');
-      });
-    });
+  if ( $('nav').css('display') != 'none')
+    activateNav();
 });
