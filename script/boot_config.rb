6d5273055d6748dbc8449c2d519ded8d758c687f
Fernando Mendes <devfrmendes@gmail.com>
2014-10-11 20:33:30 +0100

    Adds Java 8 summary

--- script/boot_config.rb
+++
@@ -1,9 +0,0 @@
-# Configuration file.
-# Constants used through out the scripts
-
-module BootConfig
-  TEMPLATE = 'script/template.slim'
-  TARGET = 'index.html'
-  DEPENDS = %w(sass slim tilt)
-  ROOT = 'Code-Week-DI-14'
-end
